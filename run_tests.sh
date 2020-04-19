539972b841539394cd3167533ab9b4d31a281fae
Okadarien Saru <okadas@dev.sysnk.net>
2020-04-19 22:54:09 +0900

    test script updated for travis

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,6 @@
 
 test -d bats-core-1.1.0/bin && PATH=$PWD/bats-core-1.1.0/bin:$PATH
 
-for i in test/*.bats; do bats $i; done
+test "$1" = "--travis" && for i in test/*.bats; do 
+  test $i != test/cook.bats && bats $i; done
+test "$1" = "--travis" || for i in test/*.bats; do bats $i; done
